360ab8825c25fe666e8c9f6952bfb7b4ac5bdb5c
destiny creates <dronesforaliving@gmail.com>
2018-07-25 13:59:29 -0400

    started the initilizer

---
+++ intersteam/game.swift
@@ -0,0 +1,29 @@
+//
+//  game.swift
+//  intersteam
+//
+//  Created by Chris Cook on 7/25/18.
+//  Copyright © 2018 destinycreates. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var title: String
+    var genre: String
+    var availability: Bool = true
+    var dueDate: Date!
+    
+    init(title: String, genre: String)
+        self.title = title
+        self.genre = genre
+    }
+}
+
+
+
+
+
+
+
+
